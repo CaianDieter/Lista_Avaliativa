f9b344a11116c446a19fce17f592d2e84257c9ef
CaianDieter <caiancaco@gmail.com>
2023-09-15 11:40:28 -0300

    vlw

--- Lista avaliativa 1/questao1.c
+++ Lista avaliativa 1/questao1.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 
-int(int xo){
+int xo(int ano){
+ int xo;
     for(xo = 1896 ,xo <= 2022, xo = xo + 4){
   }
   return xo;
 }
-int(int xc){
+int xc(int ano2){
+    int xc;
     for(xc = 1930 ,xc <= 2022,xc = xc +4){
+        if(ano2  ){
+
+        }
     }
     return xc;
 }
@@ -15,12 +20,12 @@ int main(){
 
     int X;
     scanf("%d",&X);
-    if(X == xo && X == xc){
+    if(xo(X) && xc(X)){
     printf("sss");
-    }else if(X == xc){
+    }else if(X == xc()){
     printf("sss");
 
-    }else if(X == xo){
+    }else if(X == xo()){
         printf("Os jogos Olimpicos de verao ocorreram no ano de %d",X);
 
     }

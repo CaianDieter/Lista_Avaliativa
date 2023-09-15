bcdcb3f430b063fb541b201f5d1e030501bad43f
CaianDieter <caiancaco@gmail.com>
2023-09-15 10:50:18 -0300

    first commit

---
+++ questao1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int(int xo){
+    for(xo = 1896 ,xo <= 2022, xo = xo + 4){
+  }
+  return xo;
+}
+int(int xc){
+    for(xc = 1930 ,xc <= 2022,xc = xc +4){
+    }
+    return xc;
+}
+
+int main(){
+
+    int X;
+    scanf("%d",&X);
+    if(X == xo && X == xc){
+    printf("sss");
+    }else if(X == xc){
+    printf("sss");
+
+    }else if(X == xo){
+        printf("Os jogos Olimpicos de verao ocorreram no ano de %d",X);
+
+    }
+    
+
+
+    return 0;
+}

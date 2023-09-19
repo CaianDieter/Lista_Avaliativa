121e7257375f6b6768e956d0ba3b0d276d49c57f
Caian <caiancaco@gmail.com>
2023-09-19 20:06:54 -0300

    ss

--- Lista avaliativa 1/questao2.c
+++ Lista avaliativa 1/questao2.c
@@ -1,14 +1,25 @@
-#include<stdio>
+#include<stdio.h>
+
 
 int main(){
-    int n,soma;
+    int n;
+    int soma = 0;
+
     scanf("%d",&n);
-    
+    if(n % 2 == 0){
+        printf("%d eh par\n",n);
+    }else{
+        printf("%d eh impar\n",n);
+    }
 
+    int seguraonaqui = n;
 
+    while(n > 0){
+        soma += n % 10;
+        n /= 10;
+    }
+    printf("A soma dos algarismos de %d eh %d",seguraonaqui,soma);
 
-
-    
 
     return 0;
 }
---
+++ Lista avaliativa 1/questao3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int senha,confirmacao;
+
+    scanf("%d",&senha);
+    printf("senha cadastrada: %d \n",senha);
+
+    
+
+    while(confirmacao != senha){
+    
+    scanf("%d",&confirmacao);
+
+    if(senha != senha){
+        printf("senha invalida!\n");
+
+    }else{
+        printf("senha valida!");
+    }
+
+    }
+
+    return 0;
+}

c55856cf8ee6325913badcbec660126c2129ad35
CaianDieter <caiancaco@gmail.com>
2023-09-19 10:04:10 -0300

    boar

--- Lista avaliativa 1/questao1.c
+++ Lista avaliativa 1/questao1.c
@@ -1,36 +1,35 @@
 #include<stdio.h>
 
-int xo(int ano){
- int xo;
-    for(xo = 1896 ,xo <= 2022, xo = xo + 4){
-  }
-  return xo;
-}
-int xc(int ano2){
-    int xc;
-    for(xc = 1930 ,xc <= 2022,xc = xc +4){
-        if(ano2  ){
-
-        }
+int copa(int xc){
+    for(xc = 1930;xc <= 2022;xc = xc +4){
+        printf(" ");
     }
     return xc;
 }
+int olim(int xo){
+    for(xo = 1896;xo <= 2022;xo = xo +4){
+        printf(" ");
+    }
+    return xo;
+}
+
 
 int main(){
+    int ano;
+    scanf("%d",&ano);
 
-    int X;
-    scanf("%d",&X);
-    if(xo(X) && xc(X)){
-    printf("sss");
-    }else if(X == xc()){
-    printf("sss");
+    int anocopa = copa(ano); 
+    int anoolim = olim(ano);
+      
+    if(anocopa = ano){
+        printf("A Copa do Mundo de Futebol ocorreu no ano de %d",ano);
 
-    }else if(X == xo()){
-        printf("Os jogos Olimpicos de verao ocorreram no ano de %d",X);
+    }else if(anoolim = ano){
+        printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d",ano);
 
+    }else if(anocopa != ano && anoolim != ano){
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d",ano);
     }
     
-
-
     return 0;
 }
